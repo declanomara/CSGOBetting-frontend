ae349197f96eb75f33c7b05c4528f60c803c0397
declanomara <25646795+declanomara@users.noreply.github.com>
2024-02-06 22:52:21 -0800

    Fixed hover issue with buttons in BettingControls

--- src/components/BettingControls.js
+++ src/components/BettingControls.js
@@ -119,7 +119,7 @@ const BetComponent = ( match ) => {
                     {betPlaced ? (
                         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', width: '100%' }}>
                             <span style={{ color: 'white', marginTop: "auto", marginBottom: "10px"}}>You have bet ${betInfo[1]} on <span style={{color: betSideColor}}>{match.competitors[betInfo[0]-1]}</span></span>
-                            <button style={{ color: '#f44336', backgroundColor: 'transparent', border: '1px solid #f44336', padding: '10px', borderRadius: '5px', width: '100%', margin: "2px", transform: 'translateY(0)', transition: 'transform 0.2s' }} onClick={handleCancelBet} onMouseDown={e => e.target.style.transform = 'translateY(2px)'} onMouseUp={e => e.target.style.transform = 'translateY(0)'}>Cancel Bet</button>
+                            <button style={{ color: '#f44336', backgroundColor: 'transparent', border: '1px solid #f44336', padding: '10px', borderRadius: '5px', width: '100%', margin: "2px", transform: 'translateY(0)', transition: 'transform 0.2s', cursor: 'pointer' }} onClick={handleCancelBet} onMouseDown={e => e.target.style.transform = 'translateY(2px)'} onMouseUp={e => e.target.style.transform = 'translateY(0)'}>Cancel Bet</button>
                         </div>
                     ) : (
                         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', width: '100%' }}>
@@ -127,8 +127,8 @@ const BetComponent = ( match ) => {
                                 <span style={{ color: 'white', marginRight: '5px' }}>$</span>
                                 <input type="text" style={{width: '100%', backgroundColor: '#333', border: '1px solid #444', padding: '10px', borderRadius: '5px', color:"white"}} value={betAmount} onChange={e => setBetAmount(e.target.value)} placeholder="Enter bet amount" />
                             </div>
-                            <button style={{ color: '#de9b35', backgroundColor: 'transparent', border: '1px solid #de9b35', padding: '10px', borderRadius: '5px', width: '100%', margin: "2px", transform: 'translateY(0)', transition: 'transform 0.2s' }} onClick={() => handlePlaceBet(1)} onMouseDown={e => e.target.style.transform = 'translateY(2px)'} onMouseUp={e => e.target.style.transform = 'translateY(0)'}>Bet on {match.competitors[0]}</button>
-                            <button style={{ color: '#5d79ae', backgroundColor: 'transparent', border: '1px solid #5d79ae', padding: '10px', borderRadius: '5px', width: '100%', margin: "2px", transform: 'translateY(0)', transition: 'transform 0.2s' }} onClick={() => handlePlaceBet(2)} onMouseDown={e => e.target.style.transform = 'translateY(2px)'} onMouseUp={e => e.target.style.transform = 'translateY(0)'}>Bet on {match.competitors[1]}</button>
+                            <button style={{ color: '#de9b35', backgroundColor: 'transparent', border: '1px solid #de9b35', padding: '10px', borderRadius: '5px', width: '100%', margin: "2px", transform: 'translateY(0)', transition: 'transform 0.2s', cursor: 'pointer' }} onClick={() => handlePlaceBet(1)} onMouseDown={e => e.target.style.transform = 'translateY(2px)'} onMouseUp={e => e.target.style.transform = 'translateY(0)'}>Bet on {match.competitors[0]}</button>
+                            <button style={{ color: '#5d79ae', backgroundColor: 'transparent', border: '1px solid #5d79ae', padding: '10px', borderRadius: '5px', width: '100%', margin: "2px", transform: 'translateY(0)', transition: 'transform 0.2s', cursor: 'pointer' }} onClick={() => handlePlaceBet(2)} onMouseDown={e => e.target.style.transform = 'translateY(2px)'} onMouseUp={e => e.target.style.transform = 'translateY(0)'}>Bet on {match.competitors[1]}</button>
                         </div>
                     )}
                 </>
